7d132336ca2ff91fb0d275c0db3772300997f712
CHRiS <sonxe@hotmail.hu>
2022-12-03 00:38:01 +0100

    Uploaded project

---
+++ ForditoprogBead/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace ForditoprogBead
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.convertedInputText_TB = new System.Windows.Forms.TextBox();
+            this.table_DGW = new System.Windows.Forms.DataGridView();
+            this.outputWindow = new System.Windows.Forms.RichTextBox();
+            this.ok_BT = new System.Windows.Forms.Button();
+            this.InputText_TB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.convert_BT = new System.Windows.Forms.Button();
+            this.okStepByStep_BT = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.importTbl_BT = new System.Windows.Forms.Button();
+            this.deleteTbl_BT = new System.Windows.Forms.Button();
+            this.saveOutput_BT = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.table_DGW)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // convertedInputText_TB
+            // 
+            this.convertedInputText_TB.Location = new System.Drawing.Point(321, 460);
+            this.convertedInputText_TB.Name = "convertedInputText_TB";
+            this.convertedInputText_TB.Size = new System.Drawing.Size(242, 20);
+            this.convertedInputText_TB.TabIndex = 0;
+            // 
+            // table_DGW
+            // 
+            this.table_DGW.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table_DGW.Location = new System.Drawing.Point(13, 13);
+            this.table_DGW.Name = "table_DGW";
+            this.table_DGW.Size = new System.Drawing.Size(747, 322);
+            this.table_DGW.TabIndex = 1;
+            // 
+            // outputWindow
+            // 
+            this.outputWindow.Location = new System.Drawing.Point(13, 355);
+            this.outputWindow.Name = "outputWindow";
+            this.outputWindow.Size = new System.Drawing.Size(302, 198);
+            this.outputWindow.TabIndex = 2;
+            this.outputWindow.Text = "";
+            // 
+            // ok_BT
+            // 
+            this.ok_BT.Location = new System.Drawing.Point(321, 486);
+            this.ok_BT.Name = "ok_BT";
+            this.ok_BT.Size = new System.Drawing.Size(242, 23);
+            this.ok_BT.TabIndex = 3;
+            this.ok_BT.Text = "OK";
+            this.ok_BT.UseVisualStyleBackColor = true;
+            this.ok_BT.Click += new System.EventHandler(this.ok_BT_Click);
+            // 
+            // InputText_TB
+            // 
+            this.InputText_TB.Location = new System.Drawing.Point(321, 368);
+            this.InputText_TB.Name = "InputText_TB";
+            this.InputText_TB.Size = new System.Drawing.Size(242, 20);
+            this.InputText_TB.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.OliveDrab;
+            this.label1.Location = new System.Drawing.Point(318, 444);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Converted Input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(321, 352);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Input";
+            // 
+            // convert_BT
+            // 
+            this.convert_BT.Location = new System.Drawing.Point(321, 394);
+            this.convert_BT.Name = "convert_BT";
+            this.convert_BT.Size = new System.Drawing.Size(242, 23);
+            this.convert_BT.TabIndex = 7;
+            this.convert_BT.Text = "Convert";
+            this.convert_BT.UseVisualStyleBackColor = true;
+            this.convert_BT.Click += new System.EventHandler(this.convert_BT_Click);
+            // 
+            // okStepByStep_BT
+            // 
+            this.okStepByStep_BT.Location = new System.Drawing.Point(321, 515);
+            this.okStepByStep_BT.Name = "okStepByStep_BT";
+            this.okStepByStep_BT.Size = new System.Drawing.Size(242, 23);
+            this.okStepByStep_BT.TabIndex = 8;
+            this.okStepByStep_BT.Text = "OK ( Step By Step )";
+            this.okStepByStep_BT.UseVisualStyleBackColor = true;
+            this.okStepByStep_BT.Click += new System.EventHandler(this.okStepByStep_BT_Click);
+            // 
+            // importTbl_BT
+            // 
+            this.importTbl_BT.Location = new System.Drawing.Point(610, 368);
+            this.importTbl_BT.Name = "importTbl_BT";
+            this.importTbl_BT.Size = new System.Drawing.Size(111, 23);
+            this.importTbl_BT.TabIndex = 9;
+            this.importTbl_BT.Text = "Import table";
+            this.importTbl_BT.UseVisualStyleBackColor = true;
+            this.importTbl_BT.Click += new System.EventHandler(this.importTbl_BT_Click);
+            // 
+            // deleteTbl_BT
+            // 
+            this.deleteTbl_BT.Location = new System.Drawing.Point(610, 397);
+            this.deleteTbl_BT.Name = "deleteTbl_BT";
+            this.deleteTbl_BT.Size = new System.Drawing.Size(111, 23);
+            this.deleteTbl_BT.TabIndex = 10;
+            this.deleteTbl_BT.Text = "Delete table";
+            this.deleteTbl_BT.UseVisualStyleBackColor = true;
+            this.deleteTbl_BT.Click += new System.EventHandler(this.deleteTbl_BT_Click);
+            // 
+            // saveOutput_BT
+            // 
+            this.saveOutput_BT.Location = new System.Drawing.Point(610, 515);
+            this.saveOutput_BT.Name = "saveOutput_BT";
+            this.saveOutput_BT.Size = new System.Drawing.Size(111, 23);
+            this.saveOutput_BT.TabIndex = 11;
+            this.saveOutput_BT.Text = "Save Output (.txt)";
+            this.saveOutput_BT.UseVisualStyleBackColor = true;
+            this.saveOutput_BT.Click += new System.EventHandler(this.saveOutput_BT_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(763, 579);
+            this.Controls.Add(this.saveOutput_BT);
+            this.Controls.Add(this.deleteTbl_BT);
+            this.Controls.Add(this.importTbl_BT);
+            this.Controls.Add(this.okStepByStep_BT);
+            this.Controls.Add(this.convert_BT);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InputText_TB);
+            this.Controls.Add(this.ok_BT);
+            this.Controls.Add(this.outputWindow);
+            this.Controls.Add(this.table_DGW);
+            this.Controls.Add(this.convertedInputText_TB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.table_DGW)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox convertedInputText_TB;
+        private System.Windows.Forms.DataGridView table_DGW;
+        private System.Windows.Forms.RichTextBox outputWindow;
+        private System.Windows.Forms.Button ok_BT;
+        private System.Windows.Forms.TextBox InputText_TB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button convert_BT;
+        private System.Windows.Forms.Button okStepByStep_BT;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button importTbl_BT;
+        private System.Windows.Forms.Button deleteTbl_BT;
+        private System.Windows.Forms.Button saveOutput_BT;
+    }
+}
+
---
+++ ForditoprogBead/Form1.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace ForditoprogBead
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog ofd = new OpenFileDialog();
+
+        bool debugmode = false;
+        bool stepByStep = false;
+        int steps = 0;
+
+        //input variables
+        string inputFile;
+        string inputFileName = "bemenet.txt";
+
+        string input;
+        string stack;
+        string numbers;
+
+        string[] splitInputColumn;
+        string[] splitInputRow;
+        int inputIndex;
+
+        // output variables
+        string outputFile = "";
+        string outputFileName = @"C:\Users\sonxe\source\repos\ForditoprogBead\ForditoprogBead\bin\Debug\kimenet.txt";
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            ReadFromFile("bemenet.txt");
+            Read();
+        }
+
+        void ReadFromFile(string fileName)
+        {
+            StreamReader sr = new StreamReader(File.OpenRead(@fileName));
+            inputFile = sr.ReadToEnd();
+            sr.Close();
+        }
+
+        void Read()
+        {
+            splitInputColumn = inputFile.Split('\n');
+
+            // terminális jelek hozzáadása az első sorhoz.
+            for (int i = 0; i < splitInputColumn.Length; i++)
+            {
+                splitInputRow = splitInputColumn[i].Split(';');
+                if(i == 0)
+                {
+                    for (int j = 0; j < splitInputRow.Length; j++)
+                    {
+                        table_DGW.Columns.Add(splitInputRow[j], splitInputRow[j]);
+                        if (debugmode)
+                            MessageBox.Show("Hozzáadott érték: " + splitInputRow[j]);
+                    }
+                }
+                    
+
+                table_DGW.Rows.Add();
+
+                // nem terminális jelek az első oszlopba.
+                for (int j = 0; j < splitInputRow.Length; j++)
+                {
+                    if (j == 0)
+                    {
+                        if (0 < i )
+                            table_DGW.Rows[i].HeaderCell.Value = splitInputRow[j];
+                            if (debugmode)
+                            MessageBox.Show("Hozzáadott érték: " + splitInputRow[j]);
+                    }else
+                    {
+                        //Beolvasott táblázat elemei.
+                        table_DGW[j - 1, i].Value = splitInputRow[j];
+                        if(debugmode)
+                        MessageBox.Show("i értéke: " + i + " j értéke: " + j + " Hozzáadott érték: " + splitInputRow[j]);
+                    }
+                } 
+
+            }
+            table_DGW.Rows.RemoveAt(0);
+        }
+
+        void clearTable()
+        {
+            table_DGW.Rows.Clear();
+            table_DGW.Columns.Clear();
+        }
+
+        void Write()
+        {
+            StreamWriter sw = new StreamWriter(File.OpenWrite(outputFileName));
+            sw.Write(outputFile);
+            sw.Flush();
+            sw.Close();
+        }
+
+
+        public void ok_BT_Click(object sender, EventArgs e)
+        {
+            int rowIndex = -1;
+            int columnIndex = -1;
+            outputWindow.Text = "";
+            numbers = "";
+
+            stack = table_DGW.Rows[0].HeaderCell.Value.ToString() + "#";
+            input = convertedInputText_TB.Text;
+            
+
+            if (input[input.Length - 1] != '#')
+            {
+                input = input + "#";
+                convertedInputText_TB.Text = input;
+            
+            }
+
+            string actStack = stack[0].ToString();
+            WriteFormattedOutputWindow(outputWindow, input, stack, numbers);
+
+            if (debugmode)
+            {
+                MessageBox.Show("stack: " + stack);
+                MessageBox.Show("input: " + input);
+                MessageBox.Show("Actual stack:" + actStack);
+            }
+
+            bool isCorrect = false;
+            bool isError = false;
+            
+           while (!isCorrect || !isError )
+           {
+               columnIndex = -1;
+               rowIndex = -1;
+       
+               if(stack[1].ToString() == "'")
+               {
+                   actStack = stack[0].ToString() + stack[1].ToString();
+                   if (debugmode)
+       
+                       MessageBox.Show("Actual stack: " + actStack);
+               }
+               else
+               {
+                   actStack = stack[0].ToString();
+               }
+       
+               for (int i = 0; i < table_DGW.Rows.Count-1; i++)
+               {
+                   if(actStack == table_DGW.Rows[i].HeaderCell.Value.ToString())
+                   {
+                       rowIndex = i;
+                       if (debugmode)
+                           MessageBox.Show(table_DGW.Rows[i].HeaderCell.Value.ToString() + " =  " + actStack + " Megvan. -> Sor:[" + i + "]");
+                   }
+               }
+       
+               if(rowIndex == -1)
+               {
+                    WriteFormattedOutputWindow(outputWindow,input, actStack);
+                    break;
+               }
+               
+               // 1. sorban keresünk fent a terminális jelek között.
+       
+               for (int i = 0; i < table_DGW.ColumnCount; i++)
+               {
+                   if (input[0].ToString() == table_DGW.Columns[i].Name.ToString())
+                   {
+                       columnIndex = 0;
+                       if (debugmode)
+                           MessageBox.Show(table_DGW.Columns[i].Name.ToString() + " =  " + input[0].ToString() + " Megvan. -> OSZLOP:["+i+"]");
+                   }
+               }
+       
+               // ha nem találtuk meg a terminális jelet.
+               if(columnIndex == -1)
+               {
+                   WriteFormattedOutputWindow(outputWindow, input, 0);
+                   break;
+               }
+       
+               string rule = table_DGW[input[0].ToString(), rowIndex].Value.ToString();
+       
+               if (debugmode)
+               {
+                   MessageBox.Show("Input: " + input); // i
+                   MessageBox.Show("" + table_DGW[input[0].ToString(), rowIndex].Value.ToString());
+                   MessageBox.Show(rule);
+               }
+
+                switch (rule)
+                {
+                    case "-":
+                        {
+                            WriteFormattedOutputWindow(outputWindow, input,stack,numbers);
+                            WriteFormattedOutputWindow(outputWindow, input, 0);
+                            break;
+                        }
+               
+                    case "pop":
+                        {
+                            input = input.Substring(1);
+                            stack = stack.Substring(1);
+                            WriteFormattedOutputWindow(outputWindow, input, stack, numbers);
+                            break;
+                        }
+               
+                        default:
+                        {
+                            try
+                            {
+                                numbers += rule.Split(',')[1].Substring(0, 1);
+                                if((rule.Split(',')[0])[1] == '$')
+                                {
+                                    if(stack[1].ToString() == "'")
+                                    {
+                                        stack = stack.Substring(2);
+                                    }
+                                    else
+                                    {
+                                        stack = stack.Substring(1);
+                                    }
+                                    WriteFormattedOutputWindow(outputWindow, input, stack, numbers);
+                                }
+                                else
+                                {
+                                    if(stack[1].ToString() == "'")
+                                    {
+                                        stack = rule.Split(',')[0].Substring(1) + stack.Substring(2);
+                                    }
+                                    else
+                                    {
+                                        stack = rule.Split(',')[0].Substring(1) + stack.Substring(1);
+                                    }
+                                    WriteFormattedOutputWindow(outputWindow, input, stack, numbers);
+                                }
+               
+                            }
+                            catch
+                            {
+                                WriteFormattedOutputWindow(outputWindow, input, 0 , rowIndex);
+                            }
+                            break;
+                        }
+                }
+                if (stack == "#" && input == stack)
+                {
+                    WriteFormattedOutputWindow(outputWindow, input, stack, numbers);
+                    outputWindow.AppendText("\nELFOGADVA.");
+                    isCorrect = true;
+                    break;
+                }
+               
+                if(rule == "-")
+                {
+                    isError = true;
+                    MessageBox.Show("A program hibára futott.");
+                    break;
+                }
+
+                if (stepByStep)
+                {
+                    steps++;
+                    MessageBox.Show(steps + ".lépés");
+                }
+
+
+            }
+
+            if (debugmode)
+                MessageBox.Show("A program lefutott..");
+
+            stepByStep = false;
+            steps = 0;
+        }
+
+        void ClearTable()
+        {
+            table_DGW.Columns.Clear();
+            table_DGW.Rows.Clear();
+        }
+
+        private void convert_BT_Click(object sender, EventArgs e)
+        {
+            convertedInputText_TB.Text = Regex.Replace(InputText_TB.Text, "[0-9]+", "i") + "#";
+        }
+        
+        void WriteFormattedOutputWindow(RichTextBox window, string input, int i)
+        {
+            window.Text += string.Format("\nHiba a(z) {0} karakternél.", input[i]);
+
+        }
+        void WriteFormattedOutputWindow(RichTextBox window, string input, string i)
+        {
+            int c = i[0];
+            window.Text += string.Format("\nHiba a(z) {0} karakternél.", input[c]);
+
+        }
+        void WriteFormattedOutputWindow(RichTextBox window, string input, int i, int rowIndex)
+        {
+            window.Text += string.Format("\nA {0} oszlop, {1}. sorának utasítása hibás", input[i].ToString(), rowIndex);
+        }
+        void WriteFormattedOutputWindow(RichTextBox window, string input, int i, string numbers)
+        {
+            outputWindow.Text += string.Format("\nHiba a(z) {0} karakternél.", input[i]);
+        }
+        void WriteFormattedOutputWindow(RichTextBox window, string input, string stack, string numbers)
+        {
+            window.AppendText(input + "," + stack + "," + numbers + "\n");
+        }
+        void WriteFormattedOutputWindow(RichTextBox window, string input, string stack, int numbers)
+        {
+            window.AppendText(input + "," + stack + "," + numbers + "\n");
+        }
+
+        private void okStepByStep_BT_Click(object sender, EventArgs e)
+        {
+            stepByStep = true;
+            ok_BT_Click(sender,e);
+        }
+
+        private void importTbl_BT_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "Text|*.txt*.*";
+            ofd.ShowDialog();
+            StreamReader sr = new StreamReader(File.OpenRead(ofd.FileName));
+            inputFile = sr.ReadToEnd();
+            ClearTable();
+            Read();
+        }
+
+        private void deleteTbl_BT_Click(object sender, EventArgs e)
+        {
+            ClearTable();
+        }
+
+        private void saveOutput_BT_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(outputFileName, String.Empty);
+            StreamWriter writer = File.AppendText(outputFileName);
+            writer.Write(outputWindow.Text);
+            writer.Close();
+            MessageBox.Show("Output fájl mentve " + outputFileName + " helyre/néven." );
+        }
+    }
+}
